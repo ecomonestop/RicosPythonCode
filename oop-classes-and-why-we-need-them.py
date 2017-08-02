9eb46b2ddccdc6768c5740cc407dd8a98a3720ee
Romero <212604352@logon.ds.ge.com>
2017-08-02 10:38:58 -0400

    example of class and instance attributes

--- oop-classes-and-why-we-need-them.py
+++ oop-classes-and-why-we-need-them.py
@@ -21,15 +21,31 @@ print(studentsWithAddMethod)
 # also, it shows __str__ as a way to override the "toString" method.  toString from Java seems to be equivlent to __str__ in python
 students = []
 class Student:
+    #Example of a class Attribute.  This is the same accross all instances
+    school_name = "Bates Elementary"
+
     def __init__(self, name, id=1):
-        student = {"fName": name, "id": id}
-        students.append(student)
+        #name and student_id are instance attributes.  They change with each instance.  init them here in __init__ makes the available to other methods of the class for the duration of the instnace life
+        self.name = name
+        self.student_id = id
+        students.append(self)
     
     def __str__(self):
-        return "Student"
+        return "Student " + self.name
+
+    def get_name_capitalized(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        #note you still have to use self in order to access class attributes within the class as shown below
+        return self.school_name
 
 myBetterStudent = Student("Rico")
 print(myBetterStudent)
+#notice you can access class attributes without a particular instance i.e. same as Java's classes static variables
+print(Student.school_name)
+
+
 
 
 
